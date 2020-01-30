89b16431a7d583930d55e69b26f0d6af8a01e78a
Victor Rodrigues <victorsalles1997@gmail.com>
2020-01-30 14:34:14 -0300

    second

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+    console.log('ok');
+})
+
+async function mobileMenu() {
+    const menu = document.createElement('div');
+    document.querySelector();
+
+}
